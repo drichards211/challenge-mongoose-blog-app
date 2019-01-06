eb30913e2cde76dd76eb7bb1cf367d3f2b75573d
Daniel Richards <drichards211@yahoo.com>
2019-01-06 16:12:42 -0500

    add POST request

--- server.js
+++ server.js
@@ -17,6 +17,21 @@ app.get('/blog-posts', (req, res) => {
   res.json(BlogPosts.get())
 })
 
+app.post('/blog-posts', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['title', 'content', 'author', 'publishDate']
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i]
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message)
+      return res.status(400).send(message)
+    }
+  }
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate)
+  res.status(201).json(item)
+})
+
 app.delete('/blog-posts/:id', (req, res) => {
   BlogPosts.delete(req.params.id)
   console.log(`Deleted blog post \`${req.params.id}\``)
